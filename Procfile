2a8c718be0aa66898959ff19e7b0d9979ca919d7
Joe Wegner <joe@wegnerdesign.com>
2014-03-06 16:01:25 -0600

    in-file documentation

--- Procfile
+++ Procfile
@@ -1,1 +1,2 @@
 web: node forever.js --migrations
+
--- db/index.js
+++ db/index.js
@@ -1,3 +1,25 @@
+/**
+ * This is where database migrations live.
+ * Please put your migrations in reverse numerical order, so that the newest migrations live on top.
+ *
+ * As a description of the fields:
+ *	version: The version number of the database. Just increment by 1 every time. 
+ *		Sub versions (x.x or x.x.x) will just add unnecessary complexity. Integers only.
+ *	description: Just a simple sentence explaining what your migration does. It's not used for anything
+ *		other than logging, but it will make this list more readable
+ *	migration: This is the actual file that contains the migration code.
+ *		PLEASE store your migrations in /db/migrations. Your migrations file MUST export a run function.
+ *		The run function will be called when the migration is run. It will be passed a callback function
+ *		The callback should be called with a single parameter - error.  So, like, callback("this is an error");
+ *		If there is no error, please return null. So like, callback(null);
+ *		You can look at initialize.js for a simple example.
+ *
+ *
+ *
+ * PLEASE do your best to keep this file clean. This will grow exponentially over time, and in order to keep the
+ * rest of us sane, it needs to stay somewhat readable.
+**/
+
 var migrations = exports.migrations = [
     {
         version: 1,
@@ -10,6 +32,12 @@ var migrations = exports.migrations = [
         migration: require(__dirname+"/migrations/initialize.js")
     }
 ]
+
+
+/**
+ * Please stop editing now. This stuff is the actual code that makes migrations happen.
+ * If you're just adding a new migration, there is no reason to edit below this line
+**/
 
 exports.runMigrationsSince = function(currentVersion, cb) {
 	var migrationsToDo = [];
@@ -25,6 +53,7 @@ exports.runMigrationsSince = function(currentVersion, cb) {
 	console.log("Running "+migrationsToDo.length+" database migrations");
 
 	var max = migrationsToDo.length;
+	//we need this is a first-class variable, so that it can reference itself.  This, my friend, is async recursion.
 	var migrationDone = function(err, newVersion) {
 		if(err) {
 			console.log("Migration error... Canceling the rest of the migrations");
